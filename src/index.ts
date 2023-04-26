67aa3aa89577aa651074325cc42be5922a65f7e2
nick thomas <nichoth@gmail.com>
2023-04-26 11:10:32 -0700

    setup tests

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,38 @@
-export function example ():void {
-    console.log('hello')
+import Blake2s from 'blake2s-js'
+import { fromString, toString } from 'uint8arrays'
+
+let join, readFile
+if (process.env._?.includes('node')) {
+    join = (await import('node:path')).join
+    readFile = (await import('node:fs/promises')).readFile
+}
+
+interface Args {
+    dir:string
+}
+
+interface BlobStore {
+    add: (data:Uint8Array|string) => string,
+    get: (hash:string) => Promise<Buffer>
+}
+
+export function Blobs ({ dir }:Args):BlobStore {
+    if (!dir) throw new Error('need a directory argument')
+
+    const blake = new Blake2s(32)
+
+    return {
+        add: function (_data) {
+            let data = _data
+            if (typeof _data === 'string') data = fromString(_data)
+
+            blake.update(data as Uint8Array)
+
+            return toString(blake.digest())
+        },
+
+        get: function (hash) {
+            return readFile(join(dir, hash))
+        }
+    }
 }
--- test/index.ts
+++ test/index.ts
@@ -1,5 +1,13 @@
 import { test } from 'tapzero'
+import { Blobs } from '@nichoth/blobs'
+import path from 'node:path'
+import { fileURLToPath } from 'url'
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
-test('example', async t => {
-    t.ok('ok', 'should be an example')
+test('create an instance', t => {
+    const blobs = Blobs({ dir: __dirname })
+    t.ok(blobs, 'should create a new blobs')
+    t.ok(blobs.add, 'has .add')
+    t.ok(blobs.get, 'has .get')
 })
